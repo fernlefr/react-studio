c9fd1cf53091911a821afa02d7d16a13fe6da6bf
fernlefr <varisa_tantivess@brown.edu>
2022-11-10 22:20:28 -0500

    React lab done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import { BakeryItem } from "./components/BakeryItem"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -12,19 +13,47 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  //counters
+  const [cart, updateCart] = useState(0);
+  const [items, updateItems] = useState([]);
+
+  //increments counter by 1
+  //modifiers gloobal counter state
+  // function handleClick(){
+  //   setCounter(counter + 1);
+  // }
+
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
+
+      
+     {/* <p>My counter: {counter}</p>
+      <button onClick={()=> handleClick()}>Click me!</button>  */}
+     
+     
+
+      
+
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+
+        <BakeryItem name = {item.name} description ={item.description} price ={item.price} cart={cart}
+        updateCart = {updateCart} items = {items} updateItems = {updateItems} image = {item.image} /> // replace with BakeryItem component
+       
       ))}
+
+
 
       <div>
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        <div>
+        {items.map(a => (<p>${a[0]} for {a[1]}</p>))}
+        </div>
+        <h> Total: ${cart} </h>
       </div>
-    </div>
+      </div>
   );
 }
 
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,24 @@
 // TODO: create a component that displays a single bakery item
+export function BakeryItem(props,addCounter){
+    const handleClick = () => {
+        {props.updateCart((prevCount) => prevCount + props.price)}
+        {props.items.push([props.price, props.name])}
+    }
+
+    return(
+        
+        //component
+        <div class = "Bakery Item">
+         
+        <h>{props.name}</h>
+        <p>{props.description}</p>
+        <p>{props.price}</p>
+       
+         
+
+           <img src = {props.image}/>
+           <button onClick = {handleClick} class = "button"> Add to cart!</button>
+
+        </div>
+    );
+}
